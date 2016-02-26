3ba974460b643309ac660dc97f444808f0ba1ab9
Andras Ratz <ratz.andras86@gmail.com>
2016-02-26 14:37:28 +0100

    add REST api view

--- Form/Type/DurationType.php
+++ Form/Type/DurationType.php
@@ -29,46 +29,61 @@ class DurationType extends HookType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('startDate', new DaterangepickerType(), array(
-                'label' => 'Start',
+        if ('rest' == $this->view) {
+            $builder->add('startDate', 'datetime', array(
+                    'widget' => 'single_text',
+                    'date_format' => 'Y-m-d\TH:i:sP',
+                ))
+                ->add('endDate', 'datetime', array(
+                    'widget' => 'single_text',
+                    'date_format' => 'Y-m-d\TH:i:sP',
+                ))
+                ->add('timezone', 'hidden', array(
+                    'data' => 'UTC',
+                ))
+            ;
+        } else {
+            $builder
+                ->add('startDate', new DaterangepickerType(), array(
+                    'label' => 'Start',
 //                'data' => $options['data']->getStartDate(),
-                'model_timezone' => 'UTC',
-                'view_timezone' => $this->datetime->getUserTimezone(),
-                'widget' => 'single_text',
-                'format' => $this->datetime->getUserDatetimeFormat(),
-                'date_format' => $this->datetime->getUserDateFormat(),
-                'input' => 'datetime',
-                'attr' => array(
-                    'placeholder' => 'Select a date range',
-                    'input_group' => array(
-                        'append' => '<span class="fa fa-calendar">',
-                    ),
-                )
-            ))
-            ->add('endDate', new DaterangepickerType(), array(
-                'label' => 'End',
+                    'model_timezone' => 'UTC',
+                    'view_timezone' => $this->datetime->getUserTimezone(),
+                    'widget' => 'single_text',
+                    'format' => $this->datetime->getUserDatetimeFormat(),
+                    'date_format' => $this->datetime->getUserDateFormat(),
+                    'input' => 'datetime',
+                    'attr' => array(
+                        'placeholder' => 'Select a date range',
+                        'input_group' => array(
+                            'append' => '<span class="fa fa-calendar">',
+                        ),
+                    )
+                ))
+                ->add('endDate', new DaterangepickerType(), array(
+                    'label' => 'End',
 //                'data' => $options['data']->getEndDate(),
-                'model_timezone' => 'UTC',
-                'view_timezone' => $this->datetime->getUserTimezone(),
-                'widget' => 'single_text',
-                'format' => $this->datetime->getUserDatetimeFormat(),
-                'date_format' => $this->datetime->getUserDateFormat(),
-                'input' => 'datetime',
+                    'model_timezone' => 'UTC',
+                    'view_timezone' => $this->datetime->getUserTimezone(),
+                    'widget' => 'single_text',
+                    'format' => $this->datetime->getUserDatetimeFormat(),
+                    'date_format' => $this->datetime->getUserDateFormat(),
+                    'input' => 'datetime',
 //                'read_only' => true,
-                'attr' => array(
+                    'attr' => array(
 //                    'placeholder' => 'Will be filled automatically',
-                    'is_end_date' => true,
-                    'input_group' => array(
-                        'append' => '<span class="fa fa-calendar">',
-                    ),
-                    // TODO: Decide how to display/deal with user timezone vs. campaign timezone.
+                        'is_end_date' => true,
+                        'input_group' => array(
+                            'append' => '<span class="fa fa-calendar">',
+                        ),
+                        // TODO: Decide how to display/deal with user timezone vs. campaign timezone.
 //                    'help_text' => 'Timezone: '.$options['data']->getEndDate()->getTimezone()->getName(),
-                )
-            ))
-            ->add('timezone', 'hidden', array(
-                'data' => $this->datetime->getUserTimezone(),
-            ));
+                    )
+                ))
+                ->add('timezone', 'hidden', array(
+                    'data' => $this->datetime->getUserTimezone(),
+                ));
+        }
     }
 
     public function getName()
